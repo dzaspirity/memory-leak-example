29972f48064317b99f8ab087f18e02622adb27e6
Denis Zavershinskiy <zaverden@gmail.com>
2016-12-09 20:27:57 +0700

    without memory leak

---
+++ MemoryLeakExample/BigItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MemoryLeakExample
+{
+    public class BigItem
+    {
+        public const int Size = 100;
+
+        public string Body { get; } = string.Empty.PadRight(Size);
+    }
+}
--- MemoryLeakExample/Program.cs
+++ MemoryLeakExample/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace MemoryLeakExample
 {
@@ -10,6 +6,10 @@ namespace MemoryLeakExample
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                BigItem bigItem = new BigItem();
+            }
         }
     }
 }
